b3c48b31c9e8274b3f13be52c1af8dafdb09c1cb
Julija Lee <lee.julija@gmail.com>
2017-03-22 05:56:16 +0000

    added comments to header file

--- holberton.h
+++ holberton.h
@@ -4,28 +4,27 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <stdint.h>
-
+/* functions in _putchar.c */
 int _putchar(char);
 int _strlen(char *);
 char *_strcpy(char *dest, char *src);
 void _strprint(char *str);
-
+/* functions in cpy_string.c */
 int cpy_rev(va_list list, char *buffer, int index);
 int cpy_char(va_list list, char *buffer, int index);
 int cpy_string(va_list list, char *buffer, int index);
 int cpy_bin(va_list list, char *buffer, int index);
 int cpy_rot13(va_list list, char *buffer, int index);
-
+/* functions in cpy_int.c */
 int cpy_int(va_list list, char *buffer, int index);
 int cpy_ui(va_list list, char *buffer, int index);
 int cpy_HEX(va_list list, char *buffer, int index);
 int cpy_hex(va_list list, char *buffer, int index);
 int cpy_oct(va_list list, char *buffer, int index);
-
+/* functions in cpy_special.c */
 int cpy_special(va_list list, char *buffer, int index);
-
+/* functons in _printf.c */
 int _printf(const char *format, ...);
 void clear_buffer(char *);
 int buffer_full(char *);
